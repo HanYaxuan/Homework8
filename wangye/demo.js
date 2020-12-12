28bf41db4e04007b254ab5c84f00d9c88d282da6
HanYaxuan <729655430@qq.com>
2020-12-12 21:46:56 +0800

    add

---
+++ wangye/demo.js
@@ -0,0 +1,15 @@
+/*
+* @Author: dell
+* @Date:   2020-09-26 15:19:17
+* @Last Modified by:   dell
+* @Last Modified time: 2020-09-26 15:32:56
+*/
+for(var i=9;i>=1;i--)
+{
+	document.write("<br>");
+	for(var j=1;j<=i;j++)
+	{
+		sum=i*j;
+		document.write(j,"*",i,"=",sum," ")
+	}
+}
